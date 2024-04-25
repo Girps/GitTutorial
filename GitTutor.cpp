bcc9aaa1595eaa6fafcc6e64f7b70d2179590237
Girps <jes20221168@gmail.com>
2024-04-24 23:47:31 -0700

    Added foo2

--- GitTutor.cpp
+++ GitTutor.cpp
@@ -4,14 +4,22 @@
 
 #include <iostream>
 
+// foo
 void foo() 
 {
     std::cout << "This is my intial commit";
+}
+
+// foo2
+void foo2() 
+{
+    std::cout << "This is not my intial commit"; 
 }
 
 
 int main()
 {
     foo(); 
+    foo2(); 
 }
 
